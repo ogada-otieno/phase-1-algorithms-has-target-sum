30a731a87b78e6f8fbba5743ac4908aa44e12274
Rodgers Ogada <ogadarodgers@gmail.com>
2022-12-09 16:16:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //len = array.length
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(N^2)
+  Reason: Finding a pair for every element in the array of size N
 */
 
 /* 
   Add your pseudocode here
+  -we need to iterate through the array and check each element in the array
+  -check each other element in the array apart from the one previously checked
+  -two of the values in the array should add up to the target
+  -if that's the case, return true
+      -else return false
 */
 
 /*
